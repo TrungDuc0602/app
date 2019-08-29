6a6c20a213fa7156ea8f13248ae1b4fd66ecb081
TrungDuc0602 <huaduc0602@gmail.com>
2019-08-29 17:34:57 +0700

    next

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_070031) do
+ActiveRecord::Schema.define(version: 2019_08_29_073858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_070031) do
     t.integer "cart_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
   create_table "products", force: :cascade do |t|

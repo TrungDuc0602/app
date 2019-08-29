22cf09b73c79f9e538da25ae0fee4f976faf5a10
TrungDuc0602 <huaduc0602@gmail.com>
2019-08-29 21:37:45 +0700

    The next 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,11 +3,10 @@ class Cart < ApplicationRecord
     
     def add_product(product_id)
         current_item = line_items.find_by_product_id(product_id)
-        current_item.quantity = 1
-        if current_item 
-            current_item.quantity += 1
+        if current_item
+          current_item.quantity += 1
         else
-            current_item = line_items.build(:product_id => product_id)
+          current_item = line_items.build(:product_id => product_id)
         end
         current_item
     end
